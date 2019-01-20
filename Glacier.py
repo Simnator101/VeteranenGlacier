6be4122d93e58fa91d9e00c7ad4397ffcc4c00d3
Simon Brouwer <simnator@gmail.com>
2019-01-20 23:15:07 +0100

    Glacier.py Functionality Split

--- Glacier.py
+++ Glacier.py
@@ -5,12 +5,13 @@ Created on Sat Dec 22 23:51:22 2018
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 __all__ = ["Glacier"]
 
 class Glacier(object):
     def __init__(self, xresolution=100, alpha=3., beta=.007, nu=10., width=1.,
-                 calving_param=.1, calving_frac=.3):
+                 calving_param=.1, calving_frac=.3, mean_gletsjer_w=3e3):
         assert(xresolution > 0)
         self.__type = 0
         self.__glaciel_l = 0.
@@ -22,12 +23,19 @@ class Glacier(object):
         self.__cal_p = calving_param
         self.__cal_f = calving_frac
         self.__dxres_dl = 1.
+        self.__buckets = np.array([], dtype=np.float)
+        self.__mean_gletsjet_w = mean_gletsjer_w
+        self.left_store = np.empty(1)
+        self.right_store = np.empty(1)
         
     def create_linear_bed(self, height, slope):
         self.__type = 1
         self.__slope = slope
         self.__b0 = height
         self.__glaciel_l = 1.
+        self.__buckets = np.array([], dtype=[('length', 'f8'), ('width', 'f8'),
+                                  ('height', 'f8'), ('slope', 'f8'),
+                                  ('dw_dh', 'f8')])
         
     def create_concave_bed(self, height, scale_length, height_adjustment=0.):
         self.__type = 2
@@ -35,15 +43,27 @@ class Glacier(object):
         self.__b0 = height
         self.__ba = height_adjustment
         self.__glaciel_l = 1.
+        self.__buckets = np.array([], dtype=[('length', 'f8'), ('width', 'f8'),
+                                  ('height', 'f8'), ('slope', 'f8'),
+                                  ('dw_dh', 'f8')])
         
     def create_custom_bed(self, positions, bed_elev):
         self.__type = 3
         self.__bed_pos = positions
         self.__bed_ele = bed_elev
         self.__glaciel_l = 1.
+        self.__buckets = np.array([], dtype=[('length', 'f8'), ('width', 'f8'),
+                                  ('height', 'f8'), ('slope', 'f8'),
+                                  ('dw_dh', 'f8')])
         # Specific object for a custom bed
-        #self.__xfit = np.linspace(.0, self.__glaciel_l, self.__xres)
+#        self.__xfit = np.linspace(0.0, max(positions), self.__xres)
+#        self.__hfit = np.interp(self.__xfit, positions, bed_elev)
         
+    def add_bucket(self, length, width, height, slope, dw_dh):
+        self.__buckets = np.append(self.__buckets,
+                                   np.array([
+                                           (length, width, height, slope, dw_dh)
+                                            ], dtype=self.__buckets.dtype))     
         
     def bed_height(self, x):
         if self.__type is 1:                    # Linear Bed
@@ -51,7 +71,7 @@ class Glacier(object):
         elif self.__type is 2:                  # Concave Bed
             return self.__ba + self.__b0 * np.exp(-(x / self.__scale_l))
         elif self.__type is 3:                  # Custom Bed
-            return np.interp(x, self.__bed_pos, self.__bed_ele)
+             return np.interp(x, self.__bed_pos, self.__bed_ele)
         return 0
     
     @property
@@ -85,7 +105,7 @@ class Glacier(object):
         elif self.__type is 3:
             xfit = np.linspace(.0, self.__glaciel_l, self.__xres)
             _tmp_h = np.array([self.bed_height(xp) for xp in xfit])
-            _slp = -np.gradient(_tmp_h, xfit)
+            _slp = -np.gradient(_tmp_h, xfit[1] - xfit[0])
             return np.trapz(_slp, xfit) / self.__glaciel_l
         return 0.
         
@@ -101,7 +121,7 @@ class Glacier(object):
                    np.exp(-self.__glaciel_l / self.__scale_l)
             return (_l_p + _r_p)
         elif self.__type is 3:
-            _old_gl = self.__glaciel_l
+            _old_gl = max(.1, self.__glaciel_l)
             dl = _old_gl / self.__xres
             # Calculate Left and Right mean s
             self.__glaciel_l = self.__glaciel_l - dl
@@ -129,7 +149,7 @@ class Glacier(object):
         self.set_length(1.0)
         
     def set_glacier_properties(self, alpha=3., beta=.007, nu=10., width=1., 
-                               calving_param=.1, calving_frac=.3):
+                               calving_param=.1, calving_frac=.3, mean_gletsjer_w=3e3):
         # self.reset_length() # Reset length because properties have changed
         self.__alpha = alpha
         self.__beta = beta
@@ -137,20 +157,31 @@ class Glacier(object):
         self.__width = width
         self.__cal_p = calving_param
         self.__cal_f = calving_frac
+        self.__mean_gletsjet_w = mean_gletsjer_w
         
     def simulate(self, timesteps, equiline_func, dt=1., calving_enabled=False,
-                 forcing=0.0, plot=False, plot_title=None, plot_forcing=False):
+                 forcing=0.0, plot=False, plot_title=None, plot_forcing=False,
+                 plot_ice_development=False):
         assert(type(forcing) is float or type(forcing) is int)
         timesteps = max(1, timesteps)
         new_l = np.full(timesteps + 1, self.__glaciel_l)
         forcings = np.zeros((timesteps, 2))
+        means = np.zeros((timesteps, 2))
         
+        # DEMO
+        self.right_store = np.empty(timesteps)
+        self.left_store= np.empty(timesteps)
         
         for ti in range(timesteps):
             # Calculate dL/dt
             r_v = 3. * self.__alpha / (2. * (1. + self.__nu * self.mean_slope)) * np.sqrt(self.__glaciel_l)
             l_v = self.__alpha * self.__nu / pow(1. + self.__nu * self.mean_slope, 2) *\
                   pow(self.__glaciel_l, 3. / 2.) * self.mean_ds_dl
+                  
+            # DEMO
+            self.right_store[ti] = r_v
+            self.left_store[ti] = l_v
+            
             mass_b = self.__beta * (self.mean_bed_height + self.mean_ice_height
                                     - equiline_func(ti * dt)) *\
                                    self.__glaciel_l * self.__width
@@ -161,12 +192,27 @@ class Glacier(object):
                 dens_f = 1000. / 917.
                 calv_fx = - self.__cal_p * wd * self.__width *\
                           np.max([self.__cal_f * self.mean_ice_height, dens_f * wd])
+                          
+            # Calculate Bucket Contribution
+            bucket_frc = np.zeros(self.__buckets.size)
+            for buc_i in range(bucket_frc.size):
+                cbuck = self.__buckets[buc_i]
+                bucket_frc[buc_i] = cbuck['width'] * (cbuck['height'] - equiline_func(ti * dt)) * cbuck['length']
+                bucket_frc[buc_i] += .5 * (cbuck['slope'] * cbuck['width'] + (cbuck['height'] - equiline_func(ti * dt)) * cbuck['dw_dh']) * cbuck['length'] ** 2
+                bucket_frc[buc_i] += 1. / 3. * cbuck['slope'] * cbuck['dw_dh'] * cbuck['length'] ** 3
+                bucket_frc[buc_i] *= self.__beta / self.__mean_gletsjet_w / self.mean_ice_height
+                # Check Scales
+                bucket_frc[buc_i] = max(0.0, bucket_frc[buc_i])
+                
+            
             # Determine new glacier length
             new_l[ti + 1] = new_l[ti] + pow(r_v - l_v, -1) *\
-                            (mass_b + forcing + calv_fx) * dt
+                            (mass_b + forcing + calv_fx + np.sum(bucket_frc)) * dt
             # Only keep track of this if we need it
             if plot_forcing is True:
                 forcings[ti] = [mass_b, calv_fx]
+            if plot_ice_development is True:
+                means[ti] = [self.mean_bed_height, self.mean_ice_height]
             self.__glaciel_l = new_l[ti + 1]
         
         if plot is True:
@@ -178,7 +224,7 @@ class Glacier(object):
             ax1.grid(True)
             if plot_title is not None and type(plot_title) is str:
                 plt.title(plot_title)
-            plt.legend()
+            plt.legend(loc=4)
             plt.show()
             
         if plot_forcing is True:
@@ -194,7 +240,23 @@ class Glacier(object):
                 plt.title(plot_title)
             plt.legend()
             plt.show()
-        
+            
+        if plot_ice_development is True:
+            f, ax1 = plt.subplots()
+            ax1.plot(dt*np.arange(timesteps, dtype=np.float), means.T[0], 'k-',
+                     label='Mean Bed Height')
+            ax1.plot(dt*np.arange(timesteps, dtype=np.float), means.T[1], 'b-',
+                     label='Mean Ice Thickness')
+            ax1.plot(dt*np.arange(timesteps, dtype=np.float),
+                     np.sum(means.T, axis=0),
+                     'b--', label='Mean Surface Height')
+            ax1.set_xlabel("Time (yr)")
+            ax1.set_ylabel("Height (m)")
+            ax1.grid(True)
+            if plot_title is not None and type(plot_title) is str:
+                plt.title(plot_title)
+            plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+            plt.show()
         return new_l
     
     def calculate_stable_state(self, starting_length, equi_height, dt=1.):
@@ -293,91 +355,34 @@ def esl_step_func(t):
     return steps[2]
     
 if __name__ is "__main__":   
-    import matplotlib.pyplot as plt
     glac = Glacier()
     lengths = np.linspace(1., 10e3, 100)
-    
-    # Test Linear Bed
-    glac.create_linear_bed(1000., .1)
-    glac.generate_profile(lengths, True, "Linear Bed")
-    
-    # Test Concave Bed
-    glac.create_concave_bed(1000., 1800.)
-    glac.generate_profile(lengths, True, "Concave Bed")
-
-    # Test Concave Bed Through Custrom Profile
-    sample_x = np.linspace(0., 10000., 10)
-    sample_y = 1000. * np.exp(-sample_x / 1800.)
-    glac.create_custom_bed(sample_x, sample_y)
-    glac.generate_profile(lengths, True, "Custom (Concave-like) Bed")
-    
-    # Test Linear Simulation
-    glac.create_linear_bed(1000., .1)
-    glac.simulate(400, lambda t: 500., plot=True)
-    print "Calculate Steady State"
-    print glac.calculate_stable_state(1., 500)
-    print "Calculate e-folding for this set from bottom and top"
-    print glac.calculate_efolding(1., 500)
-    print glac.calculate_efolding(16e3, 500) # Reverse
-     
-    #print glac.calculate_efolding()
-    
-    # Test Concave Simulation
-    glac.create_concave_bed(1000., 1800.)
-    glac.simulate(400, lambda t: 500., plot=True)
-    print "Calculate Steady State"
-    print glac.calculate_stable_state(1., 500)
-    print "Calculate e-folding for this set from bottom and top"
-    print glac.calculate_efolding(1., 500)
-    print glac.calculate_efolding(7e3, 500)
-    
-    # Simulation with Step-wise Equilibrium function
-    # Test Linear Bed
-    glac.create_linear_bed(1000., .1)
-    glac.simulate(4500, esl_step_func, plot=True, plot_title="Step-wise equilibrium on a Linear Bed")
-    # Test Concave Bed
-    glac.create_concave_bed(1000., 1800.)
-    glac.simulate(4500, esl_step_func, plot=True, plot_title="Step-wise equilibrium on a Concave Bed")
-    
-    # Simulation with Equilibrium Function
     equi_vals = np.genfromtxt(".\\ELA_Spitsbergen.txt").T
-    # Test Linear Bed
-    glac.create_linear_bed(1000., .1)
-    glac.set_length(glac.calculate_stable_state(1., equi_vals[1,0])[1])
-    glac.simulate(len(equi_vals[0]), esl_data, plot=True, plot_title="ESL Data Spitsbergen on a Linear Bed")
-    # Test Concave Bed
-    glac.create_concave_bed(1000., 1800.)
-    glac.set_length(glac.calculate_stable_state(1., equi_vals[1,0])[1])
-    exact_l = glac.simulate(len(equi_vals[0]), esl_data, plot=True, plot_title="ESL Data Spitsbergen on a Concave Bed")
-    
-    # Test calving
-    # Linear Bed
-    glac.create_linear_bed(1000., 1.4e3 / 1e4)
-    glac.simulate(400, lambda t: 500., plot=True, calving_enabled=True, plot_forcing=True, plot_title='Linear Bed with Calving Flux')
-    #glac.generate_profile(lengths, True, "Linear Bed")
-    # Concave Bed
-    glac.create_concave_bed(1000., 1800., height_adjustment=-130)
-    glac.generate_profile(lengths, True, "Concave Bed")
-    glac.simulate(400, lambda t: 200., plot=True, calving_enabled=True, plot_forcing=True, plot_title='Concave Bed with Calving Flux')
-    
-    # Test custom bed with ESL Data
-    sample_x = np.linspace(0., 10000., 10)
-    sample_y = 1000. * np.exp(-sample_x / 1800.)
-    glac.create_custom_bed(sample_x, sample_y)
-    glac.set_length(glac.calculate_stable_state(1., equi_vals[1,0])[1])
-    glac.generate_profile(lengths, True, "Custom (Concave-like) Bed")
-    approx_l = glac.simulate(len(equi_vals[0]), esl_data, plot=True, plot_forcing=True, plot_title="ESL Data Spitsbergen on a Concave Bed")
-    
-    
-    plt.Figure()
-    plt.plot(exact_l - approx_l)
-    plt.ylabel("Deviation height (m)")
-    plt.show()
     
     # Test custom bed with ESL data
     # This doesn't yet seem to work
-    c_geom= np.genfromtxt(".\\simple_data.txt", delimiter=',')
-    lengths = np.linspace(0.1, max(c_geom[1]), 1000)
-    glac.create_custom_bed(c_geom[1], c_geom[0])
+    c_geom= np.genfromtxt(".\\glacierelevationdata.txt", delimiter=',').T
+    lengths = np.linspace(0.1, c_geom[0,-2], 1000)
+#    glac.set_glacier_properties(calving_frac=.7, alpha=.5, mean_gletsjer_w=2930.)
+    glac.create_custom_bed(c_geom[0], c_geom[1])
+    
+    buckets = np.genfromtxt(".\\Glacierbuckets.csv", delimiter=';', skip_header=1)[:,1:]
+    for buc in buckets:
+        glac.add_bucket(buc[0], buc[1], buc[2], buc[5], buc[6])  
+    
+    # Calibrate the Winter values for the current Glacier length
     glac.generate_profile(lengths, True, "Veteranen Glacier Bed")
-    glac.simulate(len(equi_vals[0]), esl_data, calving_enabled=True, plot=True, plot_forcing=True, plot_title="ESL Data Spitsbergen on a Veteranen Glacier")
+    glac.set_length(10.)
+    glac.simulate(len(equi_vals[0]), lambda t : 1130.,
+                  calving_enabled=True,
+                  plot=True,
+                  plot_forcing=True,
+                  plot_ice_development=True,
+                  plot_title="ESL Data Spitsbergen on a Veteranen Glacier")
+    
+    #fig = plt.Figure()
+    #plt.plot(glac.right_store)
+    #plt.show()
+    #fig = plt.Figure()
+    #plt.plot(glac.left_store)
+    #plt.show()
